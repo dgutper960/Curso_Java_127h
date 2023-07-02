d8c1c31a9e366e09de2aa64351751066aed80e43
dgutper960 <dgutper960@outlook.es>
2023-07-02 11:31:36 +0200

    Input-Output-try-chatch (introducción)

---
+++ src/HolaMundo/InputOutputUsuario.java
@@ -0,0 +1,57 @@
+package HolaMundo;
+
+import javax.swing.*;
+
+public class InputOutputUsuario {
+    public static void main(String[] args) {
+        /**
+         * VAMOS A TOMAR UN VALOR DE ENTRADA POR PARTE DEL USUARIO
+         * --> En este ejemplo, necesitamos que el usuario ingrese un entero,
+         *     pero la API swing OPtionPane() nos proporciona un String.
+         * --> Debemos tener en cuenta que el usuario podría poner un dato no válido
+         *     esto nos llevaría a un error y la aplicación se rompe,
+         * --> Necesitamos controlar el flujo para errores previstos (bloque try - catch)
+         * --> Vamos a tomar una cadena de caracteres en una variable
+         * --> Vamos a convertirla a int con Integer en otra variable
+         * */
+        /*
+        * para el manejo de errores, si provocamos de error, vemos la Excepción qu lanza:
+        * Exception in thread "main" java.lang.NumberFormatException: For input string: "ho"
+	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+	at java.base/java.lang.Integer.parseInt(Integer.java:665)
+	at java.base/java.lang.Integer.parseInt(Integer.java:781)
+	at HolaMundo.InputOutputUsuario.main(InputOutputUsuario.java:22)
+        * */
+        // Nuestro código estará dentro del bloque try para evitar otros errores o conflictos de visibilidad
+        try {
+            // declaramos la variable String y la inicializamos con el valor de input obtenido
+            String numString = JOptionPane.showInputDialog(null, "Ingrese un número entero");
+            // declaramos la variable int y la inicializamos con la conversión a int del String anterior
+            int numInt = Integer.parseInt(numString); // valor int obtenido por input (ventana)
+            System.out.println(numInt);  // imprime el valor por consola el valor introducido
+
+            /** Si quisiera mostrar ese al usuario tras una pequeña operación  */
+            // operamos con el valor introducido
+            int resultado = numInt * 5;
+            // tenemos en cuenta que enviaremos un String
+            String mensaje = "El resultado de multiplicar el valor " + numInt + " por 5 es: " + resultado;
+            //JOptionPane.showMessageDialog(null, mensaje);
+
+            /** Imaginemos que tenemos varias funciones cuyo resultado queremos mostrar en esta ventana  */
+            // declaramos String mensaje para el resultado de cada función
+            String mensaje2 = "Mensaje para la salida de la función 2";
+            String mensaje3 = "Mensaje para la salida de la función 3";
+            String mensaje4 = "Mensaje para la salida de la función 4";
+            // creamos la ventana de salida y concatenamos los mensajes
+            JOptionPane.showMessageDialog(null, mensaje + "\n" +
+                    mensaje2 + "\n" + mensaje3 + "\n" + mensaje4);
+            // cerramos el bloque try y abrimos el catch
+        }catch (NumberFormatException e){ // ponemos la excepción que esperamos (ver resultado mostrado por consola) y la nombramos como 'e'
+            // mostramos el mensaje al usuario para que vea el problema
+            JOptionPane.showMessageDialog(null, "ERROR!!\nDede ingresar un número entero");
+            // Volvemos a invocar el main() para que se vuelva a ejecutar el código
+            main(args); // No debemos olvidar el argumento
+        }
+
+    }
+}
--- src/HolaMundo/SistemasNumericos.java
+++ src/HolaMundo/SistemasNumericos.java
@@ -3,5 +3,50 @@ package HolaMundo;
 public class SistemasNumericos {
     public static void main(String[] args) {
 
+        /**
+         * VAMOS A VER LO SISTEMAS NUMÉRICOS
+         * --> DECIMAL
+         * --> BINARIO
+         * --> OCTAL
+         * --> HEXADECIMAL
+         * Y SUS CONVERSIONES
+         * */
+        
+        int numericoDecimal = 500;
+        System.out.println("numericoDecimal = " + numericoDecimal);
+
+        // LO CONVERTIMOS A BINARIO
+        // usamos la clase Integer y su método toBinaryString()
+        System.out.println("Pasamos "+numericoDecimal+" a binario --> "+Integer.toBinaryString(numericoDecimal));
+        /** no olvidemos que el valor de esta operación, es un String*/
+
+        // PRUEBA //
+        Integer num = 56576;
+        Integer.toBinaryString(num); // se debe pasar como entrada obligatoriamente
+        // No podemos hacer directamente num.toBinaryString
+
+        /** vamos a inicializar una variable int con un valor binario (500)  */
+        int numBinario = 0b111110100; // es necesario el prefijo 0b para que lo tome como binario
+        // si lo imprimimos se va a interpretar como 500 y así lo imprime
+        System.out.println(numBinario); // lo muestra como decimal, ya que es por defecto
+
+        /** vamos a pasar el numBinario a octal */
+        System.out.println("El binario '"+numBinario+"' a octal es --> "+Integer.toOctalString(numBinario)); // valor octal 764
+
+        /** Entonces vamos a declarar un Octal y lo vamos a imprimir a decimal */
+        int numOctal = 0764; // el prefijo para octal es 0
+        System.out.println(numOctal);
+
+        /** Vamos a pasar 500 a hexadecimal */
+        System.out.println("El exadecimal de '"+numericoDecimal+"' es --> "+Integer.toHexString(numericoDecimal));
+
+        /** Para declarar un hexadecimal:  */
+        int numHexadecimal = 0x1f4; // prefijo 0x
+        System.out.println(numHexadecimal);
+
+
+
+
+
     }
 }
