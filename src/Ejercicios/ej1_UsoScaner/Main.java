c3eda869d0c00674787fc9abec59440ddcc3f089
dgutper960 <dgutper960@outlook.es>
2023-07-02 18:50:56 +0200

    Finalización 1er Bloque + ejecicio 1 completo

--- src/Ejercicios/ej1_UsoScaner/Main.java
+++ src/Ejercicios/ej1_UsoScaner/Main.java
@@ -13,23 +13,38 @@ public class Main {
             Scanner inputDatos = new Scanner(System.in);
 
             // pedimos los datos por consola
-            System.out.println("Introduzca el dato 1");
+            System.out.println("Introduzca su nombre");
             // suponiendo que vamos a recibir un String
-            String dato1 = inputDatos.nextLine();
+            String nombre = inputDatos.nextLine();
             // Seguimos pidiendo y capturando datos
-            System.out.println("Introduzca el dato 2");
-            String dato2 = inputDatos.nextLine();
+            System.out.println("Introduzca el nombre del producto");
+            String producto = inputDatos.nextLine();
 
-            System.out.println("Introduzca el dato 3");
-            String dato3 = inputDatos.nextLine();
-            // imprimimos los datos introducidos
-            System.out.println("Los datos introducidos son:" +
-                    "\n" + dato1 +
-                    "\n" + dato2 +
-                    "\n" + dato3);
+            System.out.println("Introduzca el precio (valor decimal)");
+            float precio = inputDatos.nextFloat(); // este dato debe ser float
+
+            System.out.println("Introduzca la cantidad adquirida (valor núm entero)");
+            int cantidad = inputDatos.nextInt(); // este dato debe ser un entero
+
+            System.out.println("Introduzca el iva \nej.- para un iva del 21% introduzca 0,21 ");
+            float iva = inputDatos.nextFloat();
+
+            //creamos una variable para calcular el precio bruto y total
+            float precioBruto = cantidad*precio;
+            float precioTotal = precioBruto+(precioBruto*iva);
+
+            // imprimimos en consola los datos finales
+            System.out.println("\nDATOS DE LA FACTURA:\n" +
+                    "\nNombre cliente: " + nombre +
+                    "\nProducto adquirido: " + producto +
+                    "\nPrecio producto: " + precio+
+                    "\nCantidad adquirida: " + cantidad+
+                    "\nIVA correspondiente: " + iva+
+                    "\nPrecio bruto: " + precioBruto+
+                    "\nPrecio con IVA: " + precioTotal);
         // En caso de error al introducir los datos
         }catch (Exception e){
-            System.out.println("El dato introducido no es correcto");
+            System.out.println("ERROR!!\nLos datos introducidos no son correctos \nRevise que los datos corresponden con lo que se solicita");
             main(args); // llamada recursiva a main() en caso de error
         }
     }
