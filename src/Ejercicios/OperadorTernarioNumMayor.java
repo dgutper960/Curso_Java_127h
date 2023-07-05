6eae87ac15de195548ed924ab5a14ac5c96fb0c2
dgutper960 <dgutper960@outlook.es>
2023-07-05 20:52:31 +0200

    instanceOf

---
+++ src/Ejercicios/OperadorTernarioNumMayor.java
@@ -0,0 +1,32 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * VAMOS A OBTENER POR CONSOLA 3 NÚMEROS
+ * VAMOS A DEVOLVER EL MAYOR
+ * SE DEBE USAR EL OPERADOR TERNARIO Y LA CLASE SCANNER
+ * */
+
+public class OperadorTernarioNumMayor {
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("INSERTADOS 3 NÚMEROS, SE DEVOLVERÁ EL MAYOR" +
+                "\n\nInserte el 1er número");
+        int a = num.nextInt();
+        System.out.println("Inserte el 2º número");
+        int b = num.nextInt();
+        System.out.println("Inserte el 3er número");
+        int c = num.nextInt();
+
+        /** implementamos el operador ternario */
+        // inicializamos con el mayor entre a y b
+        int maximo = a > b ? a : b;
+        // comparamos el mayor con c
+        maximo = maximo > c ? maximo : c;
+
+        System.out.println("\nEl número mayor es el "+maximo);
+    }
+}
---
+++ src/Operadores/InstanceOf.java
@@ -0,0 +1,15 @@
+package Operadores;
+
+public class InstanceOf {
+    public static void main(String[] args) {
+
+        /**
+         * PODEMOS VER LA RELACIÓN DE HERENCIA DE UN OBJETO
+         * */
+
+        /** Creamos objetos de diferentes tipos */
+        String texto= "Esto es solo para crear un objeto de String";
+        Integer num = 98;
+
+    }
+}
