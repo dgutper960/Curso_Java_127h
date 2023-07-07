0665420a2303908e066fdc099ae41cabb69622db
dgutper960 <dgutper960@outlook.es>
2023-07-07 13:37:34 +0200

    Condicionales 1.5

---
+++ src/Condicionales/SentencaSwitchCase.java
@@ -0,0 +1,29 @@
+package Condicionales;
+
+public class SentencaSwitchCase {
+    public static void main(String[] args) {
+
+        /**
+         * Veamos un ejemplo práctico con Switch
+         *
+         * --> Según el valor de una variable se evaluan los casos
+         *
+         * --> SOPORTA SOLO:
+         *      - Primitivos (char, byte, short, int)
+         *      - Clases Referencias (Char. Byte, Short, Integer)
+         *      - Enum
+         *      - String
+         * */
+
+        int num = 11;
+
+        switch (num){
+
+        }
+
+        /**
+         * ANTES DE SEGUIR CON LAS CLASES
+         * SE PROPONE TRANSCRIBIR EL EJEMPLO DE IF-NUM-DÍAS A SWITCH */
+
+    }
+}
