13afe701e3e2c25f4e3bbdcf98df7b1b0eb2117d
dgutper960 <dgutper960@outlook.es>
2023-07-08 17:30:18 +0200

    Bucles + etiquetas2

--- src/Bucles/EtiquetasEnForWhile.java
+++ src/Bucles/EtiquetasEnForWhile.java
@@ -49,11 +49,65 @@ public class EtiquetasEnForWhile {
                 System.out.println("[i = "+i+", j = "+j+"], ");
             }
         }
-
         /**
          *  TRAS ESTA SEGUNDA PRUEBA SI COMPROBAMOS QUE EFECTIVAMENTE,
          *  PUEDE HABER AMBIGÜEDAD SI NO MARCAMOS CORRECTAMENTE
          * --> El bloque if a actuado pero el brake no lo ha hecho
          * **/
+
+        System.out.println("\n==================================================================\n");
+
+        /** VEAMOS UN EJEMPLO PRÁCTICO */
+        /** Vamos a contar cuantas veces aparece un carácter en una frase */
+        String trabalenguas = "tres tristes tigres tragan trigo en un trigal";
+        char letra = 't';
+        int max = trabalenguas.length();
+        int ocurrencias = 0;
+        // recorremos el String
+        for (int i = 0; i < max; i++){
+            if (trabalenguas.charAt(i) == letra){
+                ocurrencias++;
+            }
+        }
+        System.out.println("ocurrencias = " + ocurrencias);
+
+        /** Ampliamos el problema y ahora queremos saber cuantas veces aparece una palabra dada en el texto
+         * suponemos que necesitamos un for para buscar en la frase y otro para comparar con la palabra  **/
+        String frase = "un elefante dos elefantes tres elefantes y cuatro elefantes";
+        String palabra = "elefante";
+        int maxP = palabra.length();
+        int maxF = frase.length() - maxP +1; // para optimizar la búsqueda al final de la frase
+        int count = 0;
+
+        buscar:
+        for(int i = 0; i < maxF ; i++){
+            int k = i; // necesitamos una variable auxiliar para incrementar con j sin alterar i
+            for (int j = 0; j < maxP ; j++){ // frase.charAt(k++)--> primero busca y luego incrementa
+                if(frase.charAt(k++) != palabra.charAt(j)){ // haya diferencias entre los caracteres de la frase y de la palabra
+                    continue buscar; //marcamos el continue al 1.er bucle --> Si hay diferencia itera el 1.er bucle
+                }
+            }
+            count++; // cuando no halla ninguna diferencia entre los caracteres de la frase y la palabra suma 1
+        }
+        System.out.println("Palabras iguales = " + count);
+
+        /** Veamos un ejemplo de como se puede optimizar ese código */
+        count = 0;
+        buscar2:
+        for(int i = 0; i < maxF ; ){
+            int k = i;
+            for (int j = 0; j < maxP ; j++){
+                if(frase.charAt(k++) != palabra.charAt(j)){
+                    i++; /** cuando no la encuentra hace el incremento normal */
+                    continue buscar2;
+                }
+            }
+            count++;
+            i = i + maxP; /** cuando encuentra una palabra no vuelve atrás de esa misma palabra */
+        }
+        System.out.println("Palabras iguales = " + count);
+
     }
 }
+
+
